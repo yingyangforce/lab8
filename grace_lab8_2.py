9ace1f69369cac5d94672776da2be803b05c73ac
Gene Calmer <yingyangforce@gmail.com>
2022-03-23 19:46:55 -0400

    finish 8_2

--- grace_lab8_2.py
+++ grace_lab8_2.py
@@ -1,3 +1,15 @@
 #+--------------------------+
 #| lab8_2.py - Isaiah Grace |
 #+--------------------------+
+
+# Task 2 - Print sum of all salaries
+
+datasum = 0
+
+datafile = open("lab8_data.txt")
+
+for lines in datafile:
+    members = lines.split()
+    datasum += int(members[2])
+
+print(f"Salary sum: ${datasum}.")
