01e0b35b0f90bb5f3985e180c7995357bf4978ea
Gene Calmer <yingyangforce@gmail.com>
2022-03-23 20:27:12 -0400

    finish review_1

--- grace_lab8_rev_1.py
+++ grace_lab8_rev_1.py
@@ -1,3 +1,20 @@
 #+------------------------------+
 #| lab8_rev_1.py - Isaiah Grace |
 #+------------------------------+
+
+# Review Task 1 - Print list contents w/o whitespace
+
+L1 = [
+    ['A','B','C'],
+    ['D','E','F','G'],
+    ['H','I','J','K','L'],
+    ['M','N','O'],
+    ['P','Q','R','S'],
+    ['T']
+]
+
+for lists in L1:
+    letters = "".join(char for char in lists)
+    print(letters, end='')
+
+print('\n')
