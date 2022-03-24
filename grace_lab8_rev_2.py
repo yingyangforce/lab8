b46e4d91b66fecfce070430c34ee7da21ebb0938
Gene Calmer <yingyangforce@gmail.com>
2022-03-24 02:03:31 -0400

    complete without any outside help at all

--- grace_lab8_rev_2.py
+++ grace_lab8_rev_2.py
@@ -1,3 +1,25 @@
 #+------------------------------+
 #| lab8_rev_2.py - Isaiah Grace |
 #+------------------------------+
+
+# Review Task 2 - Make 4 x 5 list from L1
+
+L1 = [
+    ['A','B','C'],
+    ['D','E','F','G'],
+    ['H','I','J','K','L'],
+    ['M','N','O'],
+    ['P','Q','R','S'],
+    ['T']
+]
+
+L2 = []
+L3 = []
+
+for sublists in L1:
+    L3 += (char for char in sublists)
+
+for i in range(0, len(L3), 5):
+    L2.append(L3[i:i+5])
+
+print(L2)
--- grace_lab8_rev_3.py
+++ grace_lab8_rev_3.py
@@ -1,3 +1,23 @@
 #+------------------------------+
 #| lab8_rev_3.py - Isaiah Grace |
 #+------------------------------+
+
+# Task 3 - Make deep copy, L3, of L1
+
+import copy
+
+L1 = [
+    ['A','B','C'],
+    ['D','E','F','G'],
+    ['H','I','J','K','L'],
+    ['M','N','O'],
+    ['P','Q','R','S'],
+    ['T']
+]
+
+L3 = copy.copy(L1)
+
+L3[2] = ['A', 'A']
+
+print(L1)
+print(L3)
