277db82ec8452858c42c2d192f360079d035f4cc
Gene Calmer <yingyangforce@gmail.com>
2022-03-23 20:08:32 -0400

    finish 8_3

--- grace_lab8_3.py
+++ grace_lab8_3.py
@@ -1,3 +1,14 @@
 #+--------------------------+
 #| lab8_3.py - Isaiah Grace |
 #+--------------------------+
+
+# Task 3 - read data into list, give 10% raise
+#          write all to new file, "lab8_updated".
+
+lab_data = open("lab8_data.txt")
+updated = open("lab8_updated.txt", 'a')
+
+for lines in lab_data:
+    lines = lines.split()
+    lines[2] = str(int(lines[2]) + (int(lines[2]) * .10))
+    updated.write(" ".join(lines) + '\n')
