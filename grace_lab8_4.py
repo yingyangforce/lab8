ac7cb8124f873fd7e814779442611b881ea24761
Gene Calmer <yingyangforce@gmail.com>
2022-03-23 20:22:09 -0400

    finish 8_4

--- grace_lab8_4.py
+++ grace_lab8_4.py
@@ -1,3 +1,18 @@
 #+--------------------------+
 #| lab8_4.py - Isaiah Grace |
 #+--------------------------+
+
+# Task 4 - Append data to lab8_data.
+
+newemps = [
+    (304, "Yulia", 91000),
+    (305, "Ruben", 88000)
+]
+
+datafile = open("lab8_data.txt", 'a')
+
+datafile.write('\n')
+
+for tuples in newemps:
+    newline = " ".join(str(member) for member in tuples)
+    datafile.write(newline + '\n')
